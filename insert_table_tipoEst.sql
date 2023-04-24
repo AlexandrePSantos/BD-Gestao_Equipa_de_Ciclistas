0757f49ca25970a541c35d652d07c0f14210ada8
Rui Alves <73641228+Ciclista26@users.noreply.github.com>
2023-04-24 16:13:01 +0100

    Create insert_table_tipoEst.sql

---
+++ insert_table_tipoEst.sql
@@ -0,0 +1,11 @@
+insert into Movimentos (idtipo, tipo) values ( 1 , 'Posicao' );
+insert into Movimentos (idtipo, tipo) values ( 2 , 'Km prova' );
+insert into Movimentos (idtipo, tipo) values ( 3 , 'Elevacao prova' );
+insert into Movimentos (idtipo, tipo) values ( 4 , 'Tempo prova' );
+insert into Movimentos (idtipo, tipo) values ( 5 , 'Velocidade maxima' );
+insert into Movimentos (idtipo, tipo) values ( 6 , 'Velocidade media' );
+insert into Movimentos (idtipo, tipo) values ( 7 , 'Calorias' );
+insert into Movimentos (idtipo, tipo) values ( 8 , 'Ritmo cardiaco maximo' );
+insert into Movimentos (idtipo, tipo) values ( 9 , 'Ritmo cardiaco medio' );
+insert into Movimentos (idtipo, tipo) values ( 10 , 'Sprints ganhos' );
+insert into Movimentos (idtipo, tipo) values ( 10 , 'Montanhas ganhas' );
