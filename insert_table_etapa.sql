5dfc4d99facb37e911787a1d2d5da1284e130f47
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-04-27 12:15:35 +0100

    Update insert_table_etapa.sql

--- insert_table_etapa.sql
+++ insert_table_etapa.sql
@@ -1,2 +1,11 @@
-insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (...);
-
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (0, 2010, 'Viseu', 'Viseu');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (1, 2010, 'Miranda do Corvo', 'Leiria');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (2, 2010, 'Marinha Grande', 'Loures');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (3, 2010, 'Santarém', 'Castelo Branco');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (4, 2010, 'Pampilhosa da Serra', 'Covilhã');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (5, 2010, 'Oliveira do Hospital', 'Guarda');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (6, 2010, 'Torre de Moncorvo', 'Bragança');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (7, 2010, 'Bragança', 'Montalegre');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (8, 2010, 'Viana do Castelo', 'Felgueiras');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (9, 2010, 'Fafe', 'Mondim de Basto');
+insert into etapa (numEtapa, anoEtapa, loc_partida, loc_chegada) values (10, 2010, 'Vila Nova de Gaia', 'Porto');
