83893e80e711a45ce232cada71d4a6cbb0b21140
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-04-27 11:33:38 +0100

    creates

--- Create_tables.sql
+++ Create_tables.sql
@@ -1,40 +1,36 @@
 -- Criacao da tabela Cliente
 CREATE TABLE tipoEst (
-    IdTipo int,
-	tipo char(25),
-	PRIMARY KEY (IdTipo)
+    IdTipo int PRIMARY KEY IDENTITY(1,1),
+	tipo varchar(25)
 );
 
 -- Criacao da tabela Conta
 CREATE TABLE etapa (
-    IdEtapa int IDENTITY(1,1),
-	numEtapa decimal(2),
+    IdEtapa int PRIMARY KEY IDENTITY(1,1),
+	numEtapa int,
     anoEtapa decimal(4),
-    loc_partida char(25),
-    loc_chegada char(25),
-	PRIMARY KEY (IdEtapa),
+    loc_partida varchar(25),
+    loc_chegada varchar(25)
 );
 
 -- Criacao da tabela titular conta
 CREATE TABLE ciclista(
-    Idcilista int,
-    nome char(25),
-    total_km decimal(15),
-    total_elevacao decimal(15),
-    maior_distancia decimal(10),
-    maior_elevacao decimal(10),
-    total_vitorias decimal(15),
-	PRIMARY KEY (Idcilista)
+    Idcilista int PRIMARY KEY IDENTITY(1,1),
+    nome varchar(25),
+    total_km decimal(15,1),
+    total_elevacao decimal(15,1),
+    maior_distancia decimal(10,1),
+    maior_elevacao decimal(10,1),
+    total_vitorias decimal(15,1)
 );
 
 -- Criacao da tabela movimentos
 CREATE TABLE estatistica(
-    IdEstatistica int ,
+    IdEstatistica int PRIMARY KEY IDENTITY(1,1),
     Idcilista int ,
     IdEtapa int ,
     IdTipo int ,
-    valor decimal(15) ,
-	PRIMARY KEY (IdEstatistica),
+    valor decimal(15,1),
     CONSTRAINT FK_Idcilista FOREIGN KEY (Idcilista) REFERENCES ciclista(Idcilista),
     CONSTRAINT FK_IdEtapa FOREIGN KEY (IdEtapa) REFERENCES etapa(IdEtapa),
     CONSTRAINT FK_IdTipo FOREIGN KEY (IdTipo) REFERENCES tipoEst(IdTipo)
