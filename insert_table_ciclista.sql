89ea35e3d3e0001581c1f7a73b48300afdf99af0
Rui Alves <73641228+Ciclista26@users.noreply.github.com>
2023-04-27 12:10:37 +0100

    upgrade inserts

---
+++ insert_table_ciclista.sql
@@ -0,0 +1,11 @@
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Artem', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Duarte Domingues', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Fábio Costa', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Frederico Figueiredo', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Julian Madrigal', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Luís Mendonça', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Mauricio Moreira', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Pedro Silva', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Rafael Reis', '0', '0', '0', '0', '0');
+insert into ciclista (nome, total_km, total_elevacao, maior_distancia, maior_elevacao, total_vitorias) values ('Sergio García', '0', '0', '0', '0', '0');
+
