a08eec10c6db487df27a73bc8487d1045dd4e781
Rui Alves <73641228+Ciclista26@users.noreply.github.com>
2023-05-13 12:54:14 +0100

    inserts

--- insert_table_tipoEst.sql
+++ insert_table_tipoEst.sql
@@ -2,14 +2,14 @@ insert into tipoEst (tipo) values ('Posicao');
 insert into tipoEst (tipo) values ('Km prova'); -- 140 a 210 alterar um por ano para 20
 insert into tipoEst (tipo) values ('Acumulado positivo'); -- 500 a 4000
 insert into tipoEst (tipo) values ('Acumulado negativo'); -- 500 a 4000
-insert into tipoEst (tipo) values ('Tempo prova'); -- ter atençao aos contra relogio
+insert into tipoEst (tipo) values ('Tempo prova'); -- ter atençao aos contra relogio que e entre 20 a 30 min, prova entre 4 a 6 horas 
 insert into tipoEst (tipo) values ('Velocidade maxima'); -- 65 a 80
 insert into tipoEst (tipo) values ('Velocidade media'); -- 35 a 40
 insert into tipoEst (tipo) values ('Calorias'); -- 2500 a 5000
 insert into tipoEst (tipo) values ('Ritmo cardiaco maximo'); -- 180 a 200
 insert into tipoEst (tipo) values ('Ritmo cardiaco medio'); -- 160 a 180
-insert into tipoEst (tipo) values ('Sprints ganhos'); -- 1 a 4
-insert into tipoEst (tipo) values ('Montanhas ganhas'); -- 1 a 4
+insert into tipoEst (tipo) values ('Sprints ganhos'); -- 0 a 2
+insert into tipoEst (tipo) values ('Montanhas ganhas'); -- 0 a 2
 insert into tipoEst (tipo) values ('Quedas durante a prova'); -- 0 a 2 
 insert into tipoEst (tipo) values ('Troca de bicicleta'); -- 0 a 2
 insert into tipoEst (tipo) values ('Troca de rodas');  -- 0 a 5
